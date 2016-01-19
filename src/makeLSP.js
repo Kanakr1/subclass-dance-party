ee03c3b320789a2e2ee6a54f1888bd52c3bc1670
boyajay <boyajay@github.com>
2016-01-19 14:59:46 -0800

    Add detection function for LSP

--- src/makeLSP.js
+++ src/makeLSP.js
@@ -15,8 +15,9 @@ makeLSP.prototype.constructor = makeLSP;
 
 makeLSP.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
+    
     makeDancer.prototype.step.call(this);
-
+    this.detect();
     var newTop = $("body").height() * Math.random();
     var newLeft = $("body").width() * Math.random();
     this.$node.animate({
@@ -24,12 +25,32 @@ makeLSP.prototype.step = function() {
       top: newTop
     }, 20000);
 
+
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
     //this.$node.toggle();
   };
 
+makeLSP.prototype.detect = function(first_argument) {
+ //creates a currPos.left and currPos.right property
+  //console.log(currPos);
+  
+  for (var i=0; i < dancers.length; i++){
+    var currPos = this.$node.offset();
+    var otherPos = dancers[i].$node.offset();
+    console.log(otherPos);
+    var leftDiff = Math.abs(currPos.left-otherPos.left);
+    var topDiff = Math.abs(currPos.top-otherPos.top);
+    //console.log(otherPos.left, otherPos.right);
+    // console.log(Math.abs(currPos.left-otherPos.left));
+    // console.log(Math.abs(currPos.top-otherPos.top));
+    if ((leftDiff<50 && topDiff<50)&& !(leftDiff===0 && topDiff===0)){
+      this.$node.toggle();
+    }
+  // body..
+  }
+};
 
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and

e01d6f9eb5255da74a77b03ca36b0aca081d8e47
boyajay <boyajay@github.com>
2016-01-19 20:39:48 -0800

    Implement conga line and toggle for line up

--- src/init.js
+++ src/init.js
@@ -1,31 +1,47 @@
 $(document).ready(function() {
+  console.log("document.ready function is running");
   window.dancers = [];
+  var aligned = false;
+  
+  var lineUpFunc = function () {
+          console.log(aligned);
+  if (!aligned){
+    for (var i = 1; i <= dancers.length; i ++){
+      var leftDist = 60*i-170;
+      var newStyle = {
+        top: 575,
+        left: leftDist 
+      };
+      dancers[i-1].$node.css(newStyle);
+      $('#lineUp').html("Split 'em up!");
+    }
+    aligned=true;
+    } else if (aligned) {
+      for (var j= 0; j < dancers.length; j ++){
+        var h = $("body").height() * Math.random()*0.7 + ($("body").height()*0.3);
+        var w = $("body").width() * Math.random() - 0.15* $("body").width();
+        //console.log(h,w);
+        dancers[j].setPosition(h, w);
+      }
+      $('#lineUp').html("Line 'em up!");
+      aligned= false;
+    }
+  };
+
+  $('#lineUp').on('click', lineUpFunc);
 
   $(".addDancerButton").on("click", function(event) {
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
     var dancerMakerFunction = window[dancerMakerFunctionName];
-    var lineUpFunc = function () {
-      console.log('hi');
-      var newStyle = {
-        top: 575
-      };
-      for (var i = 0; i <dancers.length; i ++){
-        dancers[i].$node.css(newStyle);
-      }
-    };
-
-    $('#lineUp').on('click', lineUpFunc);
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random()*.7 + ($("body").height()*.3), // randomizes y
+      $("body").height() * Math.random()*0.7 + ($("body").height()*0.3), // randomizes y
       $("body").width() * Math.random() - 0.15* $("body").width(), // x
       Math.random() * 1000
     );
 
     window.dancers.push(dancer);
     $('body').append(dancer.$node);
-    console.log(dancers);
-
   });
 });
 
